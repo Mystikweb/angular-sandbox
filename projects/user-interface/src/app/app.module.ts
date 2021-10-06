2e13e9d69077b01b61ffe4cbc20b3bebfd9fcf78
Christopher Hair <mystikweb@live.ca>
2021-10-06 17:21:57 -0400

    initializing Firebase in the application

--- projects/user-interface/src/app/app.module.ts
+++ projects/user-interface/src/app/app.module.ts
@@ -1,6 +1,28 @@
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularFireModule } from '@angular/fire/compat';
+
+import {
+  AngularFireAnalyticsModule,
+  APP_NAME,
+  APP_VERSION,
+  DEBUG_MODE as ANALYTICS_DEBUG_MODE,
+  ScreenTrackingService,
+  UserTrackingService,
+  COLLECTION_ENABLED
+} from '@angular/fire/compat/analytics';
+
+import { AngularFirestoreModule, USE_EMULATOR as USE_FIRESTORE_EMULATOR, SETTINGS as FIRESTORE_SETTINGS } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule, USE_EMULATOR as USE_STORAGE_EMULATOR } from '@angular/fire/compat/storage';
+import { AngularFireAuthModule, USE_DEVICE_LANGUAGE, USE_EMULATOR as USE_AUTH_EMULATOR } from '@angular/fire/compat/auth';
+import { AngularFireRemoteConfigModule, SETTINGS as REMOTE_CONFIG_SETTINGS, DEFAULTS as REMOTE_CONFIG_DEFAULTS } from '@angular/fire/compat/remote-config';
+import { AngularFirePerformanceModule, PerformanceMonitoringService } from '@angular/fire/compat/performance';
+import { AngularFireAuthGuardModule } from '@angular/fire/compat/auth-guard';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+
+import { environment } from '../environments/environment';
 
 import { SharedModule } from '@angular-sandbox/shared';
 
@@ -16,10 +38,35 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireStorageModule,
+    AngularFirestoreModule.enablePersistence({ synchronizeTabs: true }),
+    AngularFireAuthModule,
+    AngularFireAuthGuardModule,
+    AngularFireRemoteConfigModule,
+    AngularFireAnalyticsModule,
+    AngularFirePerformanceModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // provide modular style for AppCheck, see app.browser/server
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
   ],
-  providers: [],
+  providers: [
+    UserTrackingService,
+    ScreenTrackingService,
+    PerformanceMonitoringService,
+    { provide: FIRESTORE_SETTINGS, useValue: { ignoreUndefinedProperties: true } },
+    { provide: ANALYTICS_DEBUG_MODE, useValue: true },
+    { provide: COLLECTION_ENABLED, useValue: true },
+    { provide: USE_AUTH_EMULATOR, useValue: environment.useEmulators ? ['http://localhost:9099'] : undefined },
+    { provide: USE_FIRESTORE_EMULATOR, useValue: environment.useEmulators ? ['localhost', 8080] : undefined },
+    { provide: USE_STORAGE_EMULATOR, useValue: environment.useEmulators ? ['localhost', 9199] : undefined },
+    { provide: REMOTE_CONFIG_SETTINGS, useFactory: () => isDevMode() ? { minimumFetchIntervalMillis: 10_000 } : {} },
+    { provide: REMOTE_CONFIG_DEFAULTS, useValue: { background_color: 'red' } },
+    { provide: USE_DEVICE_LANGUAGE, useValue: true },
+    { provide: APP_VERSION, useValue: '0.0.0' },
+    { provide: APP_NAME, useValue: 'Angular' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
