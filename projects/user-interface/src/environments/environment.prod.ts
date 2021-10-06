223505ff6b2f25824f046d13e0e05a3f4dd9c564
Christopher Hair <mystikweb@live.ca>
2021-10-06 16:57:10 -0400

    More Firebase integration

--- projects/user-interface/src/environments/environment.prod.ts
+++ projects/user-interface/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  useEmulators: false,
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCvFK2-_aPzY0XgsiSzn3wMJRlUea9FPZc',
+    authDomain: 'angular-sandbox-e9709.firebaseapp.com',
+    projectId: 'angular-sandbox-e9709',
+    storageBucket: 'angular-sandbox-e9709.appspot.com',
+    messagingSenderId: '727800997661',
+    appId: '1:727800997661:web:a482eb2b6e88be9b76a3f4',
+    measurementId: 'G-F78G925LT7',
+  },
 };
